9fa46a7f594f24035597f6e04d14bb34d61f0c6a
xiaoyezao <44076439+xiaoyezao@users.noreply.github.com>
2019-11-26 21:40:30 +0800

    format the GeMoMa output to fit with EVM

---
+++ gemoma_gff3_to_evm_gff3.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Author: FT
+# @Date:   2019-11-26
+# @Last Modified by:   tao
+# @Last Modified time: 2019-11-26
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+gff1 = sys.argv[1]
+gff2 = sys.argv[2]
+
+with open(gff1, 'rb') as fi,\
+        open(gff2, 'wb') as fo:
+    Ltmp = []
+    for line in fi:        
+        if line.startswith(b'#'):
+            fo.write(line)
+            continue
+        Lline = line.strip().split(b'\t')
+        if Lline[2] == b'gene':
+            line = line.split(';')[0]
+            name = line.split('=')[1]
+            fo.write(line.replace('GAF','GeMoMa') + ';Name=' + name + '\n')
+            for line_tmp in Ltmp:
+                line_tmp = line_tmp.split(';')[0]
+                if 'prediction' in line_tmp:
+                    fo.write(line_tmp.replace('prediction','mRNA') + ';Parent=' + name + '\n')
+                    mRNA = line_tmp.split('=')[1]
+                if 'CDS' in line_tmp:
+                    fo.write(line_tmp.split('Parent')[0] + 'ID=cds_of_' + mRNA + ';Parent' + line_tmp.split('Parent')[1] + '\n')
+            Ltmp = []
+        else:
+            Ltmp.append(line.strip('\n'))
